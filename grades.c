25fe158029a5e9e149b17ea8cf24b35f210cee15
Ronnie Gertman <ronniegert2707@gmail.com>
2025-01-06 14:14:01 +0200

    fixed user function
    fixed the clone functions for argumented output as requested, added course name, changed functions accordingly.

--- grades.c
+++ grades.c
@@ -4,7 +4,7 @@
 
 struct grade_list_element{
 	int grade;
-	char* name;
+	char* course_name;
 };
 
 struct student_list_element{
@@ -30,21 +30,40 @@ struct grades{
 //}
 
 int clone_grade(void *element, void **output){
+	//clones a grade list element
 	if(element == NULL || output == NULL){
 		return 1;
 	}
-	struct grade_list_element* grade = (int *)element;
-	*output = malloc(sizeof(int)); // Allocate memory for an integer
-	if (*output == NULL) {
+	//casting
+	struct grade_list_element* grade_element =
+			(struct grade_list_element*)element;
+	//creating a new element
+	struct grade_list_element* new_grade =
+			(struct grade_list_element *)
+			malloc(sizeof(struct grade_list_element));
+	if (new_grade == NULL) {
 	    return 1; // Handle memory allocation failure
 	}
-	*(int *)(*output) = *grade; // Copy the value from grade to the allocated memory
+
+	//copying the name using memory allocation
+	char* copied_name = (char *)malloc(sizeof(grade_element->course_name));
+		if(copied_name == NULL){
+			free(new_grade);
+			return 1;
+		}
+	strcpy(copied_name, grade_element->course_name);
+	new_grade->course_name = copied_name;
+	new_grade->grade = grade_element->grade;
+
+	//casting new element to output
+	*(struct grade_list_element*)(*output) = *new_grade;
 	return 0; // Success
 }
 
 void destroy_grade(void* element){
-	//clones an element - a grade in a list of grades
-	int* grade = (int* )element;
+	//destroying memory allocated for course name, and struct
+	struct grade_list_element* grade = (struct grade_list_element*)element;
+	free(grade->course_name);
 	free(grade);
 }
 
@@ -57,9 +76,11 @@ struct list* clone_grade_list(struct list *list){
 	if(cloned_list == NULL){
 		return NULL;
 	}
+	//iterating through list, getting each element of the list
+	//adding a clone of this element to the new list
 	struct iterator* tmp = list_begin(list);
 	while(tmp != NULL){
-		int* grade = list_get(tmp);
+		struct grade_list_element* grade = list_get(tmp);
 		int res = list_push_back(cloned_list, (void *)grade);
 		if (res == 1){
 			list_destroy(cloned_list);
@@ -70,42 +91,48 @@ struct list* clone_grade_list(struct list *list){
 	return cloned_list;
 }
 
-void* clone_student(void* element){
+int clone_student(void* element, void** output){
 	//clones an element - a student in a list of students
 	if (element == NULL){
-		return NULL;
+		return 1;
 	}
+	//casting
 	struct student_list_element* student =
 			(struct student_list_element*)element;
+
 	//student element has a field which is a list, we need to clone this
 	//entire list using the clone list function created above
 	struct list* copied_grades = clone_grade_list(student->grades);
 	if(copied_grades == NULL){
-		return NULL;
+		return 1;
 	}
 	if(student->name == NULL){
 		list_destroy(copied_grades);
-		return NULL;
+		return 1;
 	}
+	//copying the name - memory allocation
 	char* copied_name = (char *)malloc(sizeof(student->name));
 	if(copied_name == NULL){
 		list_destroy(copied_grades);
-		return NULL;
+		return 1;
 	}
 	strcpy(copied_name, student->name);
 
+	//creating a new student element
 	struct student_list_element* new_student =
 			(struct student_list_element*)
 			malloc(sizeof(struct student_list_element));
 	if(new_student == NULL){
 		free(copied_name);
 		list_destroy(copied_grades);
-		return NULL;
+		return 1;
 	}
+	//copying all the fields
 	new_student->grades = copied_grades;
 	new_student->name = copied_name;
 	new_student->id = student->id;
-	return (void*)new_student;
+	*(struct student_list_element*)(*output) = *new_student;
+	return 0; // Success
 }
 
 void destroy_student(void* element){
@@ -117,11 +144,14 @@ void destroy_student(void* element){
 }
 
 struct grades* grades_init(){
+	//creating a new struct
 	struct grades* new_grades = (struct grades*)malloc(sizeof(struct grades));
 	if(new_grades == NULL){
 		return NULL;
 	}
+	//initializing size
 	new_grades->size = 0;
+	//creating an empty list of students
 	struct list* new_students = list_init(clone_student, destroy_student);
 	if(new_students == NULL){
 		free(new_grades);
@@ -130,11 +160,6 @@ struct grades* grades_init(){
 	new_grades->students = new_students;
 	return new_grades;
 }
-
-//we need to do clone and destroy user functions both for:
-//a single grade, student list element
-//in clone and destroy of student list element we need
-//to call for clone and destroy of grades
 
 void grades_destroy(struct grades *grades){
 	list_destroy(grades->students);
