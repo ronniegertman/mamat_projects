5af4cc1c26bf2871c48b6d9104617216ab771e7e
Ronnie Gertman <ronniegert2707@gmail.com>
2025-01-06 16:14:50 +0200

    coded all the requested functions
    added printing functions and average calculaitons

--- grades.c
+++ grades.c
@@ -1,6 +1,7 @@
 #include "grades.h"
 #include "linked-list.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct grade_list_element{
 	int grade;
@@ -162,27 +163,31 @@ struct grades* grades_init(){
 }
 
 void grades_destroy(struct grades *grades){
+	//freeing allocated memory for the list and for the struct
 	list_destroy(grades->students);
 	free(grades);
 }
 
-int grades_find_id(struct grades *grades, int id){
+struct student_list_element* grades_find_id(struct grades *grades, int id){
 	struct list* list = grades->students;
 	struct iterator* tmp = list_begin(list);
 	while(tmp != NULL){
 		struct student_list_element* student = list_get(tmp);
 		if (student->id == id){
-			return 0;
+			return student;
 		}
 		tmp = list_next(tmp);
 	}
-	return 1;
+	//we couldn't find a student with given id
+	return NULL;
 }
 
 int grades_add_student(struct grades *grades, const char *name, int id){
-	if(grades == NULL || grades_find_id(grades, id) == 0){
+	if(grades == NULL || grades_find_id(grades, id) != NULL){
 		return 1;
 	}
+	//no need to allocate memory for the student because
+	//list push clones the element
 	struct student_list_element* new_student;
 	new_student->id = id;
 	char* copied_name = (char *)malloc(sizeof(name));
@@ -196,13 +201,30 @@ int grades_add_student(struct grades *grades, const char *name, int id){
 		return 1;
 	}
 	new_student->name = copied_name;
+	//trying to add the student to the grades struct
 	if (list_push_back(grades->students,new_student) == 1){
 		free(copied_name);
 		list_destroy(new_student->grades);
 		return 1;
 	}
 	grades->size++;
+	//freeing aloucated memory that got cloned
+	free(copied_name);
 	return 0;
+}
+
+int grades_find_course_name(struct student_list_element* student, char* name){
+	struct list* list = student->grades;
+	struct iterator* tmp = list_begin(list);
+	while(tmp != NULL){
+		struct grade_list_element* grade = list_get(tmp);
+		if (strcmp(grade->course_name, name)){
+			return 0;
+		}
+		tmp = list_next(tmp);
+	}
+	//we couldn't find this course name
+	return 1;
 }
 
 int grades_add_grade(struct grades *grades,
@@ -210,4 +232,106 @@ int grades_add_grade(struct grades *grades,
                      int id,
                      int grade){
 
+	struct student_list_element* student = grades_find_id(grades, id);
+	if(grades == NULL || student == NULL){
+		return 1;
+	}
+	if(grade < 0 || grade > 100 ||
+			grades_find_course_name(student, name) == 1){
+			return 1;
+		}
+	struct grade_list_element* new_grade;
+	new_grade->grade = grade;
+	char* copied_name = (char *)malloc(sizeof(name));
+			if(copied_name == NULL){
+				return 1;
+			}
+	strcpy(copied_name, name);
+	new_grade->course_name = copied_name;
+
+	if (list_push_back(student->grades,new_grade) == 1){
+			free(copied_name);
+			return 1;
+		}
+	//freeing allocated memory that got cloned
+	free(copied_name);
+	return 0; //Success
 }
+
+float grades_calc_avg(struct grades *grades, int id, char **out){
+	struct student_list_element* student = grades_find_id(grades, id);
+	// setting the out to NULL as default, if all goes well it will be changed
+	out = NULL;
+	if(grades == NULL || student == NULL){
+		return -1;
+	}
+	//no courses
+	if(list_size(student->grades) == 0){
+		return 0;
+	}
+	struct list* list = student->grades;
+	struct iterator* tmp = list_begin(list);
+	//calculating average
+	int sum = 0;
+	while(tmp != NULL){
+		struct grade_list_element* grade = list_get(tmp);
+		sum += grade->grade;
+		tmp = list_next(tmp);
+	}
+
+	//setting the output to the student's name
+	char* copied_name = (char *)malloc(sizeof(student->name));
+	if(copied_name == NULL){
+		return -1;
+	}
+	strcpy(copied_name, student->name);
+	*out = copied_name;
+
+	return (float)(sum/list_size(list));
+}
+
+int grades_print_student(struct grades *grades, int id){
+	struct student_list_element* student = grades_find_id(grades, id);
+	if(grades == NULL || student == NULL){
+		return 1;
+	}
+	printf("%s %d: ", student->name, student->id);
+	struct list* list = student->grades;
+	if(list_size(list) == 0){
+		printf("\n");
+		return 0;
+	}
+	struct iterator* tmp = list_begin(list);
+	//we will print the first grade manually because of the lack of a comma
+	struct grade_list_element* first_grade = list_get(tmp);
+	printf("%s %d", first_grade->course_name, first_grade->grade);
+	tmp = list_next(tmp);
+	while(tmp != NULL){
+		struct grade_list_element* grade = list_get(tmp);
+		printf(", %s %d", grade->course_name, grade->grade);
+		tmp = list_next(tmp);
+	}
+	printf("\n");
+	return 0; //Success
+}
+
+int grades_print_all(struct grades *grades){
+	if(grades == NULL){
+		return 1;
+	}
+	struct list* list = grades->students;
+	//checking if there are any students
+	if(list_size(list) == 0){
+		return 0;
+	}
+	struct iterator* tmp = list_begin(list);
+	while(tmp != NULL){
+		struct student_list_element* student = list_get(tmp);
+		//printing student details
+		grades_print_student(grades, student->id);
+		tmp = list_next(tmp);
+	}
+	return 0; //Success
+
+}
+
