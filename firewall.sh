e7f711bc072858996d6fdbe821134b91a9271952
yair-dardik <yairdardik@gmail.com>
2025-01-27 17:57:48 +0200

    possibly faster

--- firewall.sh
+++ firewall.sh
@@ -1,30 +1,25 @@
-
 #!/bin/bash
 
 packets=$(cat)
 rules=$1
 filtered_packets=""
+
 # Read the rules line by line
 while IFS= read -r line; do
-    
-    # Remove spaces from the line
-    line="$(sed -e 's# *##g' <<<"$line")"
-    line="$(sed -e 's/#.*//g' <<<"$line")"
-    # Ignore empty lines
+    # Remove spaces and comments, and ignore empty lines
+    line=${line// /}       # Remove spaces (Bash parameter expansion)
+    line=${line%%#*}       # Remove comments
     [[ -z "$line" ]] && continue
-    # Extract fields from the line
-    A1="$(cut -d',' -f1 <<<"$line")"
-    A2="$(cut -d',' -f2 <<<"$line")"
-    A3="$(cut -d',' -f3 <<<"$line")"
-    A4="$(cut -d',' -f4 <<<"$line")"
-    # Process the packets through the firewall
-    current_packets="$(echo "$packets" | ./firewall.exe "$A1" | ./firewall.exe "$A2" | ./firewall.exe "$A3" | ./firewall.exe "$A4")"
-    #Append the result to filtered_packets
-    tmp=$(echo "$current_packets" | sed $'s#src-ip#\\nsrc-ip#g')
-    # echo $current_packets
-    filtered_packets+="$tmp"
- 
-done < "$rules"
-printf "%s\n" "$filtered_packets" | sort -n | uniq | sed 's# ##g' | sed '1{/^$/d}'
 
- 
+    # Extract fields from the line using Bash built-ins
+    IFS=',' read -r A1 A2 A3 A4 <<< "$line"
+
+    # Process the packets through the firewall
+    current_packets=$(echo "$packets" | ./firewall.exe "$A1" | ./firewall.exe "$A2" | ./firewall.exe "$A3" | ./firewall.exe "$A4")
+
+    # Append the result to filtered_packets with src-ip replaced
+    filtered_packets+=$'\n'"${current_packets//src-ip/$'\n'src-ip}"
+done < "$rules"
+
+# Remove duplicates, sort, and clean up
+printf "%s\n" "$filtered_packets" | awk 'NF' | sort -u | tr -d ' '
